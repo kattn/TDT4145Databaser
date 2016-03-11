daaa584d71944abf1df2dfa5d7d444fd25fa7abb
kattn <mgbjerke@gmail.com>
2016-03-11 14:25:30 +0100

    Created main program package and file

---
+++ Innlevering 2/treningsDagbok.java
@@ -0,0 +1,15 @@
+package treningsDagbok;
+import db_javaimplementation;
+
+public class treningsDagbok{
+
+
+
+    public void connectToDatabase(){
+        
+    }
+
+    public void main(Strings[] args){
+        connectToDatabase();
+    }
+}
