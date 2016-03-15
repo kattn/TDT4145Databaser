bd4e070ae895c9065ff712004f15b9cd520d3586
kattn <mgbjerke@gmail.com>
2016-03-15 12:39:37 +0100

    now userstory1 gets all the needed input

--- Innlevering2/Innlevering2.java
+++ Innlevering2/Innlevering2.java
@@ -6,24 +6,10 @@ public class Innlevering2{
     private static BufferedReader cin = null;
     private static String output = null;
 
-    private static boolean quitRepeatCheck(String inp, String output){
-        switch (inp){
-            case "repeat":
-                System.out.print(output);
-                break;
-            case "quit":
-                return true;
-            default:
-                break;
-        }
-        return false;
-    }
-
     private static int welcomeText() throws IOException{
         int ret = -2;
         try {
             output = "Welcome to your workout diary!\n" +
-                    "If at anytime you want to repeat the last message type repeat.\n" +
                     "If you want to quit at anytime type quit.\n" +
                     "Type in one of the following numbers to use the program, they are" +
                     "meant to mirror the use cases presented in the assignment:\n" +
@@ -41,7 +27,7 @@ public class Innlevering2{
             System.out.print(output);
             while(run){
                 inp = cin.readLine();
-                if(quitRepeatCheck(inp,output)){
+                if(inp.equals("quit")){
                     ret = -1;
                 }
                 if( 0< Integer.valueOf(inp) && Integer.valueOf(inp) <9){
@@ -56,77 +42,155 @@ public class Innlevering2{
         }
     }
 
-    private static void userStory1(){
-        output = "1 - Register a new workout and how it went";
-        System.out.print(output);
+    /*Klar til å legges til query, har en query string array av lengde 11 bestående av føgende
+    dd,mm,yyyy,hh,mm,varighetHH,varighetMM,type,vurdering,hvilepuls,vaer,tempertur */
+    private static boolean userStory1(){
+        System.out.print("1 - Register a new workout and how it went\n");
         //tar inn input til å lage query
+        System.out.print("Type in date and time(dd/mm/yyyy,hh:mm):\n");
+        try{
+            String[] query = new String[12]; //dd,mm,yyyy,hh,mm,varighetHH,varighetMM,type,vurdering,hvilepuls,vaer,tempertur
+            String tempInp = cin.readLine();
+            if((tempInp).equals("quit"))
+                return false;
+            query[0] = tempInp.split(",")[0].split("/")[0];
+            query[1] = tempInp.split(",")[0].split("/")[1];
+            query[2] = tempInp.split(",")[0].split("/")[2];
+            query[3] = tempInp.split(",")[1].split(":")[0];
+            query[4] = tempInp.split(",")[1].split(":")[1];
+            System.out.print("Type in the length of the workout(hh:mm):\n");
+            tempInp = cin.readLine();
+            if(tempInp.equals("quit"))
+                return false;
+            query[5] = tempInp.split(":")[0];
+            query[6] = tempInp.split(":")[1];
+            System.out.print("Type in type of workout:\n");
+            tempInp = cin.readLine();
+            if(tempInp.equals("quit"))
+                return false;
+            query[7] = tempInp;
+            System.out.print("Type in evaluation of workout(on a line):\n");
+            tempInp = cin.readLine();
+            if(tempInp.equals("quit"))
+                return false;
+            query[8] = tempInp;
+            System.out.print("Type in daily resting pulse:\n");
+            tempInp = cin.readLine();
+            if(tempInp.equals("quit"))
+                return false;
+            query[9] = tempInp;
+            System.out.print("Type in type of weather:\n");
+            tempInp = cin.readLine();
+            if(tempInp.equals("quit"))
+                return false;
+            query[10] = tempInp;
+            System.out.print("Type in temp of workout:\n");
+            tempInp = cin.readLine();
+            if(tempInp.equals("quit"))
+                return false;
+            query[11] = tempInp;
+
+            for(String inp: query){
+                System.out.print(inp+" ");
+            }
+
+        } catch(IOException e){
+            System.out.print(e);
+        }
+
+        return true;
     }
 
-    private static void userStory2(){
+    private static boolean userStory2(){
         output = "2 - Register a new workoutgoal, view old goals or view known exercises";
         System.out.print(output);
+        return true;
+
     }
 
-    private static void userStory3(){
+    private static boolean userStory3(){
         output = "3 - View progression for a specific exercise over a preiod and current and old goals";
         System.out.print(output);
+        return true;
     }
 
-    private static void userStory4(){
+    private static boolean userStory4(){
         output = "4 - Check the difference between a specific result and the best result reached during a" +
             "given period and the difference between the result and the current goal";
         System.out.print(output);
+        return true;
     }
 
-    private static void userStory5(){
+    private static boolean userStory5(){
         output = "5 - Copy a specific workout as a template";
         System.out.print(output);
+        return true;
     }
 
-    private static void userStory6(){
+    private static boolean userStory6(){
         output = "6 - Check the correlation between results and daily form";
         System.out.print(output);
+        return true;
     }
 
-    private static void userStory7(){
+    private static boolean userStory7(){
         output = "7 - Print all workoutnotes in a log";
         System.out.print(output);
+        return true;
     }
 
-    private static void userStory8(){
+    private static boolean userStory8(){
         output = "8 - Add, reorganize and delete exercises, groups and subgroups";
         System.out.print(output);
+        return true;
     }
 
     public static void main(String[] args){
         cin = new BufferedReader(new InputStreamReader(System.in));
         try {
             int temp = welcomeText();
+            System.out.print(temp);
             while(true) {
                 switch (temp) {
                     case 1:
-                        userStory1();
+                        if(!userStory1()){
+                            return;
+                        }
                         break;
                     case 2:
-                        userStory2();
+                        if(!userStory2()){
+                            return;
+                        }
                         break;
                     case 3:
-                        userStory3();
+                        if(!userStory3()){
+                            return;
+                        }
                         break;
                     case 4:
-                        userStory4();
+                        if(!userStory4()){
+                            return;
+                        }
                         break;
                     case 5:
-                        userStory5();
+                        if(!userStory5()){
+                            return;
+                        }
                         break;
                     case 6:
-                        userStory6();
+                        if(!userStory6()){
+                            return;
+                        }
                         break;
                     case 7:
-                        userStory7();
+                        if(!userStory7()){
+                            return;
+                        }
                         break;
                     case 8:
-                        userStory8();
+                        if(!userStory8()){
+                            return;
+                        }
                         break;
                     case -1:
                         cin.close();
