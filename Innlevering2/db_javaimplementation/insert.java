5560d44cd4c8447a0385e06945143d48f99bbd44
Fredrik Anthonisen <fredrik.anthonisen94@gmail.com>
2016-03-15 12:52:01 +0100

    pusher insert og delete java filer

---
+++ Innlevering2/db_javaimplementation/insert.java
@@ -0,0 +1,54 @@
+package db_javaimplementation;
+
+import java.sql.*;
+
+public class insertDelete {
+	
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+	static final String DB_URL = "jdbc:mysql://localhost/STUDENTS";
+
+	static final String username = "username";
+	static final String password = "password";
+	
+	public boolean insert(String tableName,ArrayList<String> array){
+		
+		Connection  connection = null;
+		Statement statement = null;
+		try {
+			Class.forName(JDBC_DRIVER);
+			
+			connection = DriverManager.getConnection(DB_URL, username, password);
+			
+			statement = connection.createStatement();
+			String sql = "INSERT INTO " + tableName + " VALUES(";
+			for(String attribute : array){
+				sql.concat(attribute);
+				sql.concat(", ");
+			}
+			sql = sql[:sql.length()];
+			sql.concat(")");
+			statement.executeUpdate(sql);
+			System.out.println("Insert into " + tableName + " complete. ")
+			return true;
+		} catch(SQLException se){
+			se.printStackTrace();
+			return false;
+		} catch(Exception e){
+			e.printStackTrace();
+			return false;
+		} finally {
+			try{
+		         if(stmt!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		      }// do nothing
+		      try{
+		         if(conn!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		         se.printStackTrace();
+		      }//end finally try
+		   }
+		}
+	
+}
